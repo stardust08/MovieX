273f2081cf35ee39ead0a09dc5a1a42031084044
Roshan <roshanprasad9650@gmail.com>
2023-10-06 02:26:04 -0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState, useEffect } from "react";
+import {fetchDataFromApi} from "./utils/api";
+function App() {
+  return (
+    useEffect
+      <div className="App">App</div>
+ 
+  );
+}
+
+export default App;
